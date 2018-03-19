2932777c157372c4d8f8d826ffecd3b1c60bf636
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-19 09:01:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function init() {
   //put any page initialization/handlebars initialization here
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form").innerHTML)
 
   Handlebars.registerHelper('displayIngredient', function() {
 
   })
 
-  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form").innerHTML)
+
 
   function createRecipe() {
 

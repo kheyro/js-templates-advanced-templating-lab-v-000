e775979771e7c0b9cf5e8eedfc84e287fdda3397
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-19 08:22:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+  Handlebars.registerHelper('displayIngredient', function() {
+    
+  })
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()

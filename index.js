f6929f96a1ee287ee18c8beee7cebbcf0edc9d99
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-19 09:48:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function createRecipe() {
 }
 
 function displayEditForm() {
-  let name = document.getElementById('recipe-name').value
+  let name = document.getElementById('recipe-name').innerText
   let description = document.getElementById('recipe-description').value
   let ingredientsList = document.getElementsByName('ingredientsList')
 

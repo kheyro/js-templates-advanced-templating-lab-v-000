bf03938eac21e79421875b924059e98b490525d6
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-19 08:52:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function createRecipe() {
 
-  let name = document.getElementById('name')
-  let ingredients = document.getElementsByName('ingredients').children.value
+  let name = document.getElementById('name').value
 
   let recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
 
